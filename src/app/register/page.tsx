169c2b79c419dc19c2b507b56c23a0e6c85a2675
khoa <user.iamjasper@gmail.com>
2024-05-22 03:09:35 +0700

    feat: register process

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,58 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const RegisterPage = () => {
+    const [formData, setFormData] = useState({
+        customer_firstname: '',
+        customer_lastname: '',
+        phone: '',
+        email: '',
+        password: '',
+        c_password: '',
+        address: '',
+    })
+
+    const [error, setError] = useState<string | null>(null)
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { id, value } = e.target
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [id]: value,
+        }))
+    }
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+
+        if (formData.password !== formData.c_password) {
+            toast.error('Mật khẩu xác nhận không trùng nhau')
+            return
+        }
+
+        try {
+            const response = await fetch('https://nguyenkim-be.onrender.com/v2/customer/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            })
+
+            const result = await response.json()
+            if (response.status === 200) {
+                toast.success('Đăng kí tài khoản thành công')
+            } else {
+                setError(result.message)
+                toast.error(error)
+            }
+        } catch (error) {
+            toast.error('Có lỗi xảy ra trong lúc tạo tài khoản')
+        }
+    }
+
     return (
         <div className="bg-gray-400 min-h-screen flex items-center justify-center">
             <div className="container mx-auto">
@@ -14,46 +66,80 @@ const RegisterPage = () => {
                         ></div>
                         <div className="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
                             <h3 className="pt-4 text-2xl text-center">Create an Account!</h3>
-                            <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded">
+                            <form onSubmit={handleSubmit} className="px-8 pt-6 pb-8 mb-4 bg-white rounded">
                                 <div className="mb-4 md:flex md:justify-between">
                                     <div className="mb-4 md:mr-2 md:mb-0">
                                         <label
                                             className="block mb-2 text-sm font-bold text-gray-700"
-                                            htmlFor="firstName"
+                                            htmlFor="customer_firstname"
                                         >
                                             First Name
                                         </label>
                                         <input
                                             className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                            id="firstName"
+                                            id="customer_firstname"
                                             type="text"
                                             placeholder="First Name"
+                                            value={formData.customer_firstname}
+                                            onChange={handleChange}
                                         />
                                     </div>
                                     <div className="md:ml-2">
                                         <label
                                             className="block mb-2 text-sm font-bold text-gray-700"
-                                            htmlFor="lastName"
+                                            htmlFor="customer_lastname"
                                         >
                                             Last Name
                                         </label>
                                         <input
                                             className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                            id="lastName"
+                                            id="customer_lastname"
                                             type="text"
                                             placeholder="Last Name"
+                                            value={formData.customer_lastname}
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="mb-4 md:flex md:justify-between">
+                                    <div className="mb-4 md:mr-2 md:mb-0">
+                                        <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="phone">
+                                            Phone
+                                        </label>
+                                        <input
+                                            className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                            id="phone"
+                                            type="text"
+                                            placeholder="Phone"
+                                            value={formData.phone}
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                    <div className="md:ml-2">
+                                        <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="email">
+                                            Email
+                                        </label>
+                                        <input
+                                            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                            id="email"
+                                            type="email"
+                                            placeholder="Email"
+                                            value={formData.email}
+                                            onChange={handleChange}
                                         />
                                     </div>
                                 </div>
                                 <div className="mb-4">
-                                    <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="email">
-                                        Email
+                                    <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="address">
+                                        Address
                                     </label>
                                     <input
                                         className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                        id="email"
-                                        type="email"
-                                        placeholder="Email"
+                                        id="address"
+                                        type="text"
+                                        placeholder="Address"
+                                        value={formData.address}
+                                        onChange={handleChange}
                                     />
                                 </div>
                                 <div className="mb-4 md:flex md:justify-between">
@@ -65,12 +151,13 @@ const RegisterPage = () => {
                                             Password
                                         </label>
                                         <input
-                                            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                                             id="password"
                                             type="password"
-                                            placeholder="******************"
+                                            placeholder="Password"
+                                            value={formData.password}
+                                            onChange={handleChange}
                                         />
-                                        <p className="text-xs italic text-red-500">Please choose a password.</p>
                                     </div>
                                     <div className="md:ml-2">
                                         <label
@@ -83,14 +170,16 @@ const RegisterPage = () => {
                                             className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                                             id="c_password"
                                             type="password"
-                                            placeholder="******************"
+                                            placeholder="Confirm Password"
+                                            value={formData.c_password}
+                                            onChange={handleChange}
                                         />
                                     </div>
                                 </div>
                                 <div className="mb-6 text-center">
                                     <button
                                         className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-                                        type="button"
+                                        type="submit"
                                     >
                                         Register Account
                                     </button>
@@ -117,6 +206,18 @@ const RegisterPage = () => {
                     </div>
                 </div>
             </div>
+            <ToastContainer
+                position="bottom-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </div>
     )
 }
